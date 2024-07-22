bc01c48c052bd11a83097eb0ac31ec24ce13a82a
ilijavuk <ilija@lynkdev.com>
2024-07-22 08:50:49 +0200

    Platform specific code - fixing SafeAreaView for android

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-import { Button, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   console.log("app executed");
@@ -19,7 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
