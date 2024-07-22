4f7d8c8430465786f67a586af9d3e1d203516d6b
ilijavuk <ilija@lynkdev.com>
2024-07-22 08:35:17 +0200

    SafeAreaView, Images, Touchables

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableOpacityComponent,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 export default function App() {
+  let x = 1;
+  console.log("app executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World!</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <TouchableHighlight
+        onPress={() => console.log("Image tapped")}
+        onLongPress={() => console.log("Image long pressed")}
+      >
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
