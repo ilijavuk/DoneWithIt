20ed3c836f006fc3ccd819aaf19959ed140d885a
ilijavuk <ilija@lynkdev.com>
2024-07-26 13:12:45 +0200

    StackNavigator

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Button from "./app/components/Button";
+import colors from "./app/config/colors";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      text="View Tweets"
+      color={colors.secondary}
+      onPress={() =>
+        navigation.navigate("TweetDetails", {
+          id: 1,
+          name: "John Doe",
+        })
+      }
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details</Text>
+    <Text>{route.params.name}</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({
+        title: route.params.name,
+      })}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <ListingEditScreen />
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
